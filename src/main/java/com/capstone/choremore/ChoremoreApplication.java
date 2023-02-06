36d409a6154150ecaaab3ebaae3651ec24a8d06e
John-Michael Krsak <jmkrsak@gmail.com>
2023-02-06 09:43:22 -0500

    first commit

---
+++ src/main/java/com/capstone/choremore/ChoremoreApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.choremore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoremoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoremoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/choremore/ChoremoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.choremore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoremoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
