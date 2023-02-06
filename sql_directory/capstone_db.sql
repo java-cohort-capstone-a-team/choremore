6dafa8107f1cce0752cfeffaff0801bb4d88cc6e
John-Michael Krsak <jmkrsak@gmail.com>
2023-02-06 09:45:08 -0500

    project structure complete, .properties complete, .sql schema complete, pom dependencies/plugins/security complete

---
+++ sql_directory/capstone_db.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS capstone_db;
+
+CREATE DATABASE IF NOT EXISTS capstone_db;
+
+USE capstone_db;
--- src/test/java/com/capstone/choremore/ChoremoreApplicationTests.java
+++ src/test/java/com/capstone/choremore/ChoremoreApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChoremoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
