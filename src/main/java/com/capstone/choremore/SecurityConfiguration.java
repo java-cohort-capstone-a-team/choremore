eddd5f1c645b93cd3a27119fa3a84384d9e00f52
John-Michael Krsak <jmkrsak@gmail.com>
2023-02-13 22:52:54 -0500

    element changes view when status is complete or incomplete

--- src/main/java/com/capstone/choremore/SecurityConfiguration.java
+++ src/main/java/com/capstone/choremore/SecurityConfiguration.java
@@ -69,7 +69,7 @@ public class SecurityConfiguration {
                 .logoutSuccessUrl("/login?logout")
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/", "/login", "/sign-up", "/css/**")
+                .requestMatchers("/", "/login", "/sign-up", "/css/**", "/js/**")
                 .permitAll()
                 .and().authorizeHttpRequests().requestMatchers("/create-avatar", "/avatar-creation", "/profile", "/chore-manager", "/avatar-manager", "/deletechore", "/editchore", "/createchore", "/approved", "/deletechild", "/approvedPro", "/deletechorePro", "/editchorePro").hasAuthority("ROLE_PARENT")
                 .and().authorizeHttpRequests().requestMatchers("/child-profile", "/chores-view", "/message-board", "/createmsg", "/skill-builder", "/hpplus", "/strengthplus", "/deletemsg", "/defenseplus", "/changestatus", "/editmsg", "/editmsg", "/editmsgPro", "/deletemsgPro", "/changestatusPro", "/deletechild", "/avatar-form", "/avatarbuilder", "/battle-arena", "/battle").hasAuthority("ROLE_CHILD")
