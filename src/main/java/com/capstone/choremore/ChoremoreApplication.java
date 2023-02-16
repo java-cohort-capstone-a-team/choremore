cc457bbc41915ad85bcff9d0c55b774629816f95
ethan <ethan.gonzalez00774@gmail.com>
2023-02-16 10:15:28 -0800

    jumbotron and font

--- src/main/java/com/capstone/choremore/ChoremoreApplication.java
+++ src/main/java/com/capstone/choremore/ChoremoreApplication.java
@@ -1,10 +1,32 @@
 package com.capstone.choremore;
 
+import com.capstone.choremore.jumboTron.StreamingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class ChoremoreApplication {
+
+
+    @Autowired
+    private StreamingService service;
+
+    @GetMapping(value = "video/{title}", produces = "video/mp4")
+    public Mono<Resource> getVideos(@PathVariable String title, @RequestHeader("Range") String range) {
+        System.out.println("YOOOOOOOO range in Bytes() : " + range);
+        return service.getVideo(title);
+    }
+
+
+
 
     public static void main(String[] args) {
 
@@ -13,3 +35,6 @@ public class ChoremoreApplication {
     }
 
 }
+
+
+
